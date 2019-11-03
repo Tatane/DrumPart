1db22eb99673b87274b390c3b493469fbc6a92cf
monom <no@mail.com>
2019-11-03 14:29:44 -0500

    Cleanup in main.cpp

--- main.cpp
+++ main.cpp
@@ -12,58 +12,30 @@ int main()
 {
     Partition partition;
 
-    /*
-    unique_ptr<Measure> measure1 = make_unique<Measure>(partition);
-            //, measure2(partition);
+    // Partition::insertMeasure returns a ref to a new created Measure if the caller didn't pass an existing Measure.
+    unique_ptr<Measure> & newMeas1 = partition.insertMeasure(0);
 
     // REMARK :
     // The Measure::addNote function has a signature that forces us to pass a rvalue-reference parameter.
     // So we can either move an already existing object (though it will have been nullified on function return), ...
-    unique_ptr<Note> note1 = make_unique<Note>(DrumNotes::Bass, 0);
-    measure1.addNote(std::move(note1));
-
-    // ... either pass a temporary object :
-    measure1.addNote(make_unique<Note>(DrumNotes::Snare, 4));
-    measure1.addNote(make_unique<Note>(DrumNotes::Bass, 6));
-    measure1.addNote(make_unique<Note>(DrumNotes::Bass, 11));
-    measure1.addNote(make_unique<Note>(DrumNotes::Snare, 12));
-    measure1.addNote(make_unique<Note>(DrumNotes::Bass, 15));
-
-    measure2 = measure1;
-
-    Measure measure3 = measure1;
-*/
-
-    //unique_ptr<Measure> meas1 = make_unique<Measure>(partition);
-    //partition->insertMeasure(0, std::move(meas1));
-
-    make_unique<Measure>(partition);
-
-    unique_ptr<Measure> & newMeas1 = partition.insertMeasure(0);
-    //unique_ptr<Measure> & newMeas2 = partition.insertMeasure(1);
-    //unique_ptr<Measure> & newMeas3 = partition.insertMeasure(2);
-    //unique_ptr<Measure> & newMeas4 = partition.insertMeasure(3);
-
+    // unique_ptr<Note> note1 = make_unique<Note>(DrumNotes::Bass, 0);
+    // measure.addNote(std::move(note1));
     newMeas1->addNote(make_unique<Note>(DrumNotes::Bass, 0));
     newMeas1->addNote(make_unique<Note>(DrumNotes::Snare, 4));
     newMeas1->addNote(make_unique<Note>(DrumNotes::Bass, 8));
     newMeas1->addNote(make_unique<Note>(DrumNotes::Snare, 12));
 
+    // REAMRK : as do Measure::addNote, Partition::insertMeasure takes a unique_ptr as parameter, thus the caller has to std::move an existing unique_ptr, or pass a temporary one.
+    // Below, we create new Measures by copying an existing one.
     unique_ptr<Measure> meas2(new Measure(*newMeas1));
     partition.insertMeasure(1, std::move(meas2));
-
     partition.insertMeasure(2, unique_ptr<Measure>(new Measure(*newMeas1)));
 
-    //newMeas2->addNote(make_unique<Note>(DrumNotes::Bass, 0));
-    //newMeas3->addNote(make_unique<Note>(DrumNotes::Bass, 0));
-    //newMeas4->addNote(make_unique<Note>(DrumNotes::Bass, 0));
 
-    //partition->addMeasure(&measure2);
-    //partition->addMeasure(&measure3);
     partition.play();
 
 
-    /*
+    /*  TODO :
 
 Ajouter une note a une mesure deja existante.
 Modifier le Type d'une note existante.
